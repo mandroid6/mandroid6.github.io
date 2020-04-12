1165c7f9870f2e8ad28ee382835b9080b2594003
Mandar Deshpande <razzormandar@gmail.com>
2020-04-12 01:49:48 -0700

    Update script.js

--- Happy_Birthday/script.js
+++ Happy_Birthday/script.js
@@ -5,3 +5,11 @@ function open_card() {
 function close_card() {
   document.getElementById('outside').className = '';
 }
+
+function open_childhood() {
+  document.getElementById('inside').className = 'open-card';
+}
+
+function close_childhood() {
+  document.getElementById('inside').className = '';
+}
